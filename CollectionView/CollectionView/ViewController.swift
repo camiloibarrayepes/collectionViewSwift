f374fa7f4be08a63ff0770cc7c52a36d1875b6e0
camilo andres ibarra yepes <cibarra@grupoalto.com>
2019-04-30 13:55:57 -0500

    init project

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by camilo andres ibarra yepes on 4/30/19.
+//  Copyright © 2019 camilo andres ibarra yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
